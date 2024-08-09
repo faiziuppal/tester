2ae1f52ae52b119601c4fbe880f5b8b85236044b
Faizanrajput1120 <90038147+Faizanrajput1120@users.noreply.github.com>
2024-08-09 04:08:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 app.use(express.json());
  
 const dbConfig = {
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  host: 'auth-db1592.hstgr.io',
+  user:'u650672385_root' ,
+  password: "_MYschool_db1",
+  database: 'u650672385__myschool_db'
 };
 
 const pool = mysql.createPool(dbConfig);
